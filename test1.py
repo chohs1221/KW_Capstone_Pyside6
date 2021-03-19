afe17a656c53ca39c36e5835931d1507079ea6aa
chohs1221 <hscho1221@naver.com>
2021-03-19 14:01:09 +0900

    first commit

---
+++ test1.py
@@ -0,0 +1,33 @@
+import random
+import sys
+
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget)
+from __feature__ import snake_case, true_property
+
+class MyWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.hello = [
+            "안녕하세요", "q"
+        ]
+
+        self.button = QPushButton("click me!")
+        self.message = QLabel("Hello World")
+        self.message.alignment = Qt.AlignCenter
+
+        self.layout = QVBoxLayout(self)
+        self.layout.add_widget(self.message)
+        self.layout.add_widget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        self.message.text = random.choice(self.hello)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MyWidget()
+    widget.show()
+    sys.exit(app.exec_())
